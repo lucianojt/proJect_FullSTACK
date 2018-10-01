0cd7460d94b1479117c37d86ad5089ec103a7f24
jidapa <onjidapa@windowslive.com>
2018-10-01 16:14:10 +0700

    edit port setting

--- server.js
+++ server.js
@@ -104,5 +104,7 @@ res.redirect('/products');
 
 
 });
-console.log('App is running at http://localhost:8080');
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+console.log('App is running on http://localhost:' + port);
+});
