06362560a09d476f62ed8f237803bde85fe33b39
jidapa tassanakul <onjidapa@windowslive.com>
2018-10-20 17:10:47 +0700

    update

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/products/:pid', function (req, res) {
 
 });
 
-app.post('/products/update', function (req, res) {
+app.post('/product/update', function (req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
