f016a8a0a9e5aee1955395c9e9eb1f13476976c7
jidapa tassanakul <onjidapa@windowslive.com>
2018-10-20 17:15:05 +0700

    update

--- server.js
+++ server.js
@@ -99,7 +99,9 @@ app.post('/product/update', function (req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
-    var sql = `update products set title =  ${title}, price = ${price} where id = ${id}`;
+    var sql = `update products 
+               set title =  '${title}', price = '${price}' 
+               where id = '${id}'`;
     console.log('UPDATE: ' + sql)
     res.redirect('/products');
 
