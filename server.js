7f851a9f8d71cc6028c67b44e89e07f52cef0f84
jidapa tassanakul <onjidapa@windowslive.com>
2018-10-20 18:26:12 +0700

    de

--- server.js
+++ server.js
@@ -144,8 +144,8 @@ app.post('/product/insert', function (req, res) {
 
 
 });
-app.post('/product/delete', function (req, res) {
-    var id = req.param('id');
+app.get('/product_delete/:id', function (req, res) {
+    var id = req.param.id;
     var sql = 'delete from products';
     if (id) {
         sql += ' whrer id =' + id;
